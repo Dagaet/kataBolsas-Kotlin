7f0a4d54a68dafb753d3b78ae83ee693f04ac14a
Dagaet <alojera15@gmail.com>
2024-04-17 09:02:36 +0100

    Test(Green): Bag should add an object when there is capacity

--- src/main/kotlin/dev/kata/bolsas/Bag.kt
+++ src/main/kotlin/dev/kata/bolsas/Bag.kt
@@ -1,8 +1,17 @@
 package dev.kata.bolsas
 
-class Bag {
-    fun execute(): Int
-    {
-        return 0
+class Bag: Bags{
+    private val size = 4
+    private val itemsList: MutableList<Pair<String, String>> = mutableListOf()
+    override fun add(item: Pair<String, String>): Boolean {
+        return itemsList.add(item)
+    }
+
+    override fun isFull(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun organizeAlphabetically(): MutableList<Pair<String, String>> {
+        TODO("Not yet implemented")
     }
 }
