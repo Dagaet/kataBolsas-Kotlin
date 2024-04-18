0fc8ddfa451ec4134b7bec756fa1f99c546303e5
Dagaet <alojera15@gmail.com>
2024-04-18 08:41:34 +0100

    Wip(Bag): Added constructor for category

--- src/main/kotlin/dev/kata/bolsas/Bag.kt
+++ src/main/kotlin/dev/kata/bolsas/Bag.kt
@@ -1,6 +1,39 @@
 package dev.kata.bolsas
 
-class Bag: Bags{
+interface Category{
+    fun itsMyElement() : Boolean
+    object metals : Category{
+        private val elements = listOf("Copper", "Gold", "Iron", "Silver")
+        override fun itsMyElement(): Boolean {
+            TODO("Not yet implemented")
+        }
+    }
+    object clothes : Category{
+        private val elements = listOf("Leather", "Linen", "Silk", "Wool")
+        override fun itsMyElement(): Boolean {
+            TODO("Not yet implemented")
+        }
+    }
+    object herbs : Category {
+        private val elements = listOf("Cherry Blossom", "Marigold", "Rose", "Seaweed")
+        override fun itsMyElement(): Boolean {
+            TODO("Not yet implemented")
+        }
+    }
+    object weapons : Category{
+        private val elements = listOf("Axe", "Dagger", "Mace", "Sword")
+        override fun itsMyElement(): Boolean {
+            TODO("Not yet implemented")
+        }
+    }
+    object noCategory : Category{
+        override fun itsMyElement(): Boolean {
+            return true
+        }
+    }
+}
+class Bag(private val category: Category): Bags{
+
     private val capacity = 4
     private val itemsList: MutableList<Pair<String, String>> = mutableListOf()
     override fun add(item: Pair<String, String>): Boolean {
